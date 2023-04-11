8ce47f1011e034ad3dfbddb1a8e062328f1c5818
text-code <dssn2102@gmail.com>
2023-04-11 19:57:42 +0900

    first commit

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,47 @@
+package web.model;
+
+public class Car {
+    private Long id;
+    private String model;
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(Long id, String model, int series) {
+        this.id = id;
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car: " + id +
+                "; Model - " + model +
+                "; Series - " + series;
+    }
+}
---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,13 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.List;
+
+@Service
+public class CarService {
+    public static List<Car> getListCars(List<Car> cars, int count) {
+        return cars.stream().limit(count).toList();
+    }
+}
