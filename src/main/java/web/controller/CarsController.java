8d0bae22338ad4684b58f37effa0500581b3fd87
text-code <dssn2102@gmail.com>
2023-04-14 18:28:20 +0900

    replace Model with ModelMap

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -2,7 +2,7 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
@@ -20,7 +20,7 @@ public class CarsController {
     @GetMapping("/cars")
     public String getCars(
             @RequestParam(value = "count", defaultValue = "5") int count,
-            ModelMap model
+            Model model
     ) {
         model.addAttribute("cars", carService.getListCars(count));
 
